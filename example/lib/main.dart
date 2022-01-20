204db9417b1d1c22ab6eddf388740d8cfc135727
student <amchishty66786@gmail.com>
2022-01-20 21:29:50 +0500

    web image view

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Firebase Chat',
       theme: ThemeData(
         primarySwatch: Colors.blue,
